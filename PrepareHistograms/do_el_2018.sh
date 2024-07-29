07e4a8e52390b199db8dd9e0dc66eccedacebea0
Ashling Quinn <aislinndream918@gmail.com>
2024-07-29 16:48:05 +0200

    trying to run do_el with 2018 crab output

--- PrepareHistograms/do_el_2018.sh
+++ PrepareHistograms/do_el_2018.sh
@@ -1,15 +1,19 @@
+# Makes histograms from data, MC
 ./Make.sh FinalSelection_el.cc
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/EGamma.root output_el_2018/EGamma_HLTMu23Ele12.root data_obs HLTMu23Ele12
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTMu23Ele12.root DY HLTMu23Ele12
 
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/EGamma.root output_el_2018/EGamma_HLTMu8Ele23.root data_obs HLTMu8Ele23
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTMu8Ele23.root DY HLTMu8Ele23
+# Four cross-triggers (HLTMu23, 8, etc)
+#-------------------string year----------------string input-----------------------------------------------------string output----------string sample--string discriminant
+./FinalSelection_el.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/EGamma_Electron_Data_2018.root output_el_2018/EGamma_HLTMu23Ele12.root data_obs HLTMu23Ele12 #three different trigger paths
+#./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTMu23Ele12.root DY HLTMu23Ele12
 
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/EGamma.root output_el_2018/EGamma_HLTEle32.root data_obs HLTEle32
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTEle32.root DY HLTEle32
+./FinalSelection_el.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/EGamma_Electron_Data_2018.root output_el_2018/EGamma_HLTMu8Ele23.root data_obs HLTMu8Ele23
+#./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTMu8Ele23.root DY HLTMu8Ele23
 
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/EGamma.root output_el_2018/EGamma_HLTEle24Tau30.root data_obs HLTEle24Tau30
-./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTEle24Tau30.root DY HLTEle24Tau30
+./FinalSelection_el.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/EGamma_Electron_Data_2018.root output_el_2018/EGamma_HLTEle32.root data_obs HLTEle32
+#./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTEle32.root DY HLTEle32
+
+./FinalSelection_el.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/EGamma_Electron_Data_2018.root output_el_2018/EGamma_HLTEle24Tau30.root data_obs HLTEle24Tau30
+#./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_HLTEle24Tau30.root DY HLTEle24Tau30
 
 #./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/EGamma.root output_el_2018/EGamma_antiiso.root data_obs antiiso
 #./FinalSelection_el.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_el_2018/DY.root output_el_2018/DY_antiiso.root DY antiiso
