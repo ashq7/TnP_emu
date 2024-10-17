22d02185caa706a83f38b021261a50e34d33bd6d
Ashling Quinn <aislinndream918@gmail.com>
2024-10-17 06:43:09 +0200

    changes that were deleted during merge conflict a few weeks ago

--- Fit/do_el_2018.sh
+++ Fit/do_el_2018.sh
@@ -2,8 +2,10 @@
 ./Make.sh Fit_el_CMSShape.cc
 
 #---------------------year----------------------input----------------------------------output---------------------------sample--discriminant 
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTMu8Ele23.root output_Data_2018_HLTMu8Ele23.root Data HLTMu8Ele23
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTMu8Ele23.root output_DY_2018_HLTMu8Ele23.root DY HLTMu8Ele23
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTMu8Ele23.root output_Data_2018_HLTMu8Ele23.root Data HLTMu8Ele23
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTMu8Ele23.root output_DY_2018_HLTMu8Ele23.root DY HLTMu8Ele23
+./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/Embedded_HLTMu8Ele23.root output_Embedded_2018_HLTMu8Ele23.root Embedded HLTMu8Ele23
+
 python ScaleFactors_el.py --year=2018 --discriminant=HLTMu8Ele23
 
 #Ashling did NOT comment this out
@@ -11,17 +13,19 @@ python ScaleFactors_el.py --year=2018 --discriminant=HLTMu8Ele23
 #./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_antiiso.root output_DY_2018_antiiso.root DY antiiso
 #python ScaleFactors_el.py --year=2018 --discriminant=antiiso
 
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTMu23Ele12.root output_Data_2018_HLTMu23Ele12.root Data HLTMu23Ele12
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTMu23Ele12.root output_DY_2018_HLTMu23Ele12.root DY HLTMu23Ele12
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTMu23Ele12.root output_Data_2018_HLTMu23Ele12.root Data HLTMu23Ele12
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTMu23Ele12.root output_DY_2018_HLTMu23Ele12.root DY HLTMu23Ele12
+./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/Embedded_HLTMu23Ele12.root output_Embedded_2018_HLTMu23Ele12.root Embedded HLTMu23Ele12
 python ScaleFactors_el.py --year=2018 --discriminant=HLTMu23Ele12
 
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTEle32.root output_Data_2018_HLTEle32.root Data HLTEle32
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTEle32.root output_DY_2018_HLTEle32.root DY HLTEle32
-python ScaleFactors_el.py --year=2018 --discriminant=HLTEle32
+#Don't need single electron or ETau cross trigger
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTEle32.root output_Data_2018_HLTEle32.root Data HLTEle32
+#./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTEle32.root output_DY_2018_HLTEle32.root DY HLTEle32
+#python ScaleFactors_el.py --year=2018 --discriminant=HLTEle32
 
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTEle24Tau30.root output_Data_2018_HLTEle24Tau30.root Data HLTEle24Tau30
-./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTEle24Tau30.root output_DY_2018_HLTEle24Tau30.root DY HLTEle24Tau30
-python ScaleFactors_el.py --year=2018 --discriminant=HLTEle24Tau30
+# ./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_HLTEle24Tau30.root output_Data_2018_HLTEle24Tau30.root Data HLTEle24Tau30
+# ./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/DY_HLTEle24Tau30.root output_DY_2018_HLTEle24Tau30.root DY HLTEle24Tau30
+# python ScaleFactors_el.py --year=2018 --discriminant=HLTEle24Tau30
 
 #Ashling did NOT comment this out
 #./Fit_el_CMSShape.exe 2018 ../PrepareHistograms/output_el_2018/EGamma_MVAisoWPLoose.root output_Data_2018_MVAisoWPLoose.root Data MVAisoWPLoose
