d91a4b6ded718dfb9883daed8557ac8b5bc95487
Ashling Quinn <aislinndream918@gmail.com>
2024-11-06 01:23:06 +0100

    running electron 2017 jobs

--- MiniAODProcessorCode/test/submit_crab_el_DY_2017.py
+++ MiniAODProcessorCode/test/submit_crab_el_DY_2017.py
@@ -1,4 +1,4 @@
-from CRABClient.UserUtilities import config, getUsernameFromCRIC
+from CRABClient.UserUtilities import config#, getUsernameFromCRIC
 
 config = config()
 
@@ -13,7 +13,7 @@ config.JobType.maxMemoryMB = 4000
 #config.JobType.numCores = 8
 
 config.Data.inputDataset = "/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL17MiniAODv2-106X_mc2017_realistic_v9-v2/MINIAODSIM" 
-config.Data.outLFNDirBase = "/store/group/cmst3/group/taug2/TnP_el_2017/"
+config.Data.outLFNDirBase = "/store/user/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/2017_el_DY"
 #config.Data.outLFNDirBase = "/store/group/cmst3/user/ccaillol/reNanoAOD/"
 config.Data.outputDatasetTag = "TnP_ntuples_el2017"
 config.Data.inputDBS = "global"
@@ -22,5 +22,5 @@ config.Data.unitsPerJob = 10
 config.Data.publication = False
 config.Data.ignoreLocality = True
 
-config.Site.storageSite = "T2_CH_CERN"
+config.Site.storageSite = "T3_CH_CERNBOX"
 config.Site.whitelist = ["T2_*","T3_*"]
