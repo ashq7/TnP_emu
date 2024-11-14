10768158d91c2cd4f6e0419e5f8c7de51452ae5b
Ashling Quinn <aislinndream918@gmail.com>
2024-11-14 22:58:58 +0100

    added embedded

--- PrepareHistograms/do_mu_2018.sh
+++ PrepareHistograms/do_mu_2018.sh
@@ -1,15 +1,23 @@
 ./Make.sh FinalSelection_mu.cc
+
+#Only need two cross triggers
+
+#HLTMu8Ele23
+#-------------------string year----------------string input----------------------------------------------string output----------string sample--string discriminant
 ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTMu8Ele23.root data_obs HLTMu8Ele23
 ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTMu8Ele23.root DY HLTMu8Ele23
+./FinalSelection_mu.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Embedded_Muon_2018_241009.root output_mu_2018/Embedded_HLTMu8Ele23.root data_obs HLTMu8Ele23
 
+#HLTMu23Ele12
 ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTMu23Ele12.root data_obs HLTMu23Ele12
 ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTMu23Ele12.root DY HLTMu23Ele12
+./FinalSelection_mu.exe 2018 /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Embedded_Muon_2018_241009.root output_mu_2018/Embedded_HLTMu23Ele12.root data_obs HLTMu23Ele12 
 
 #./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_antiiso.root data_obs antiiso
 #./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_antiiso.root DY antiiso
 
-./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTMu20Tau27.root data_obs HLTMu20Tau27
-./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTMu20Tau27.root DY HLTMu20Tau27
+# ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTMu20Tau27.root data_obs HLTMu20Tau27
+# ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTMu20Tau27.root DY HLTMu20Tau27
 
-./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTIsoMu24.root data_obs HLTIsoMu24
-./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTIsoMu24.root DY HLTIsoMu24
+# ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/SingleMuon.root output_mu_2018/SingleMuon_HLTIsoMu24.root data_obs HLTIsoMu24
+# ./FinalSelection_mu.exe 2018 /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2018/DY.root output_mu_2018/DY_HLTIsoMu24.root DY HLTIsoMu24
