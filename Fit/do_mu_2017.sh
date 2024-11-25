2fdef33f70fd65f0d6619b8a0806af19064ba223
Ashling Quinn <aislinndream918@gmail.com>
2024-11-25 22:50:17 +0100

    was missing the output_mu_2017 folder which caused seg fault

--- Fit/do_mu_2017.sh
+++ Fit/do_mu_2017.sh
@@ -1,11 +1,11 @@
 ./Make.sh Fit_mu.cc
-./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/SingleMuon_HLTMu8Ele23.root output_Data_2017_HLTMu8Ele23.root Data HLTMu8Ele23
-./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/DY_HLTMu8Ele23.root output_DY_2017_HLTMu8Ele23.root DY HLTMu8Ele23
+./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/SingleMuon_HLTMu8Ele23.root output_mu_2017/output_Data_2017_HLTMu8Ele23.root Data HLTMu8Ele23
+./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/DY_HLTMu8Ele23.root output_mu_2017/output_DY_2017_HLTMu8Ele23.root DY HLTMu8Ele23
 ./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/Embedded_HLTMu8Ele23.root output_mu_2017/output_Embedded_2017_HLTMu8Ele23.root Embedded HLTMu8Ele23
 python ScaleFactors_mu.py --year=2017 --discriminant=HLTMu8Ele23
 
-./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/SingleMuon_HLTMu23Ele12.root output_Data_2017_HLTMu23Ele12.root Data HLTMu23Ele12
-./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/DY_HLTMu23Ele12.root output_DY_2017_HLTMu23Ele12.root DY HLTMu23Ele12
+./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/SingleMuon_HLTMu23Ele12.root output_mu_2017/output_Data_2017_HLTMu23Ele12.root Data HLTMu23Ele12
+./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/DY_HLTMu23Ele12.root output_mu_2017/output_DY_2017_HLTMu23Ele12.root DY HLTMu23Ele12
 ./Fit_mu.exe 2017 ../PrepareHistograms/output_mu_2017/Embedded_HLTMu23Ele12.root output_mu_2017/output_Embedded_2017_HLTMu23Ele12.root Embedded HLTMu23Ele12
 python ScaleFactors_mu.py --year=2017 --discriminant=HLTMu23Ele12
 
