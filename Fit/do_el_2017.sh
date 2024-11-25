442d5bfdcdd02a0796313d58ca6ac1ba8df883d7
Ashling Quinn <aislinndream918@gmail.com>
2024-11-25 20:44:42 +0100

    fixed Data file typo

--- Fit/do_el_2017.sh
+++ Fit/do_el_2017.sh
@@ -1,13 +1,13 @@
 ./Make.sh Fit_el.cc
 ./Make.sh Fit_el_CMSShape.cc
 
-./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/SingleElectron_HLTMu8Ele23.root output_el_2017/output_Data_2017_HLTMu8Ele23.root Data HLTMu8Ele23
+./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/Data_HLTMu8Ele23.root output_el_2017/output_Data_2017_HLTMu8Ele23.root Data HLTMu8Ele23
 ./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/DY_HLTMu8Ele23.root output_el_2017/output_DY_2017_HLTMu8Ele23.root DY HLTMu8Ele23
 ./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/Embedded_HLTMu8Ele23.root output_el_2017/output_Embedded_2017_HLTMu8Ele23.root Embedded HLTMu8Ele23
 python ScaleFactors_el.py --year=2017 --discriminant=HLTMu8Ele23
 
 
-./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/SingleElectron_HLTMu23Ele12.root output_el_2017/output_Data_2017_HLTMu23Ele12.root Data HLTMu23Ele12
+./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/Data_HLTMu23Ele12.root output_el_2017/output_Data_2017_HLTMu23Ele12.root Data HLTMu23Ele12
 ./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/DY_HLTMu23Ele12.root output_el_2017/output_DY_2017_HLTMu23Ele12.root DY HLTMu23Ele12
 ./Fit_el_CMSShape.exe 2017 ../PrepareHistograms/output_el_2017/Embedded_HLTMu23Ele12.root output_el_2017/output_Embedded_2017_HLTMu23Ele12.root Embedded HLTMu23Ele12
 python ScaleFactors_el.py --year=2017 --discriminant=HLTMu23Ele12
