4e7e5de5279634e28a74f943bb2e1a7be884a319
Ashling Quinn <aislinndream918@gmail.com>
2024-12-12 06:17:38 +0100

    edited prepare histograms scripts for 2016

--- PrepareHistograms/do_el_2016post.sh
+++ PrepareHistograms/do_el_2016post.sh
@@ -1,12 +1,12 @@
 ./Make.sh FinalSelection_el.cc
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/SingleElectron.root output_el_2016post/SingleElectron_HLTMu23Ele12.root data_obs HLTMu23Ele12
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/DY.root output_el_2016post/DY_HLTMu23Ele12.root DY HLTMu23Ele12
+./FinalSelection_el.exe 2016post /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Data_Electron_2016post.root output_el_2016post/Data_HLTMu23Ele12.root data_obs HLTMu23Ele12
+./FinalSelection_el.exe 2016post /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/DY_Electron_2016post.root output_el_2016post/DY_HLTMu23Ele12.root DY HLTMu23Ele12
 
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/SingleElectron.root output_el_2016post/SingleElectron_HLTMu8Ele23.root data_obs HLTMu8Ele23
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/DY.root output_el_2016post/DY_HLTMu8Ele23.root DY HLTMu8Ele23
+./FinalSelection_el.exe 2016post /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Data_Electron_2016post.root output_el_2016post/Data_HLTMu8Ele23.root data_obs HLTMu8Ele23
+./FinalSelection_el.exe 2016post /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/DY_Electron_2016post.root output_el_2016post/DY_HLTMu8Ele23.root DY HLTMu8Ele23
 
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/SingleElectron.root output_el_2016post/SingleElectron_HLTEle25.root data_obs HLTEle25
-./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/DY.root output_el_2016post/DY_HLTEle25.root DY HLTEle25
+# ./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/SingleElectron.root output_el_2016post/SingleElectron_HLTEle25.root data_obs HLTEle25
+# ./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/DY.root output_el_2016post/DY_HLTEle25.root DY HLTEle25
 
 #./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/SingleElectron.root output_el_2016post/SingleElectron_antiiso.root data_obs antiiso
 #./FinalSelection_el.exe 2016post /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016post/DY.root output_el_2016post/DY_antiiso.root DY antiiso
--- PrepareHistograms/do_el_2016pre.sh
+++ PrepareHistograms/do_el_2016pre.sh
@@ -1,12 +1,12 @@
 ./Make.sh FinalSelection_el.cc
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/SingleElectron.root output_el_2016pre/SingleElectron_HLTMu23Ele12.root data_obs HLTMu23Ele12
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/DY.root output_el_2016pre/DY_HLTMu23Ele12.root DY HLTMu23Ele12
+./FinalSelection_el.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Data_Electron_2016pre.root output_el_2016pre/Data_HLTMu23Ele12.root data_obs HLTMu23Ele12
+./FinalSelection_el.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/DY_Electron_2016pre.root output_el_2016pre/DY_HLTMu23Ele12.root DY HLTMu23Ele12
 
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/SingleElectron.root output_el_2016pre/SingleElectron_HLTMu8Ele23.root data_obs HLTMu8Ele23
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/DY.root output_el_2016pre/DY_HLTMu8Ele23.root DY HLTMu8Ele23
+./FinalSelection_el.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Data_Electron_2016pre.root  output_el_2016pre/Data_HLTMu8Ele23.root data_obs HLTMu8Ele23
+./FinalSelection_el.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/DY_Electron_2016pre.root output_el_2016pre/DY_HLTMu8Ele23.root DY HLTMu8Ele23
 
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/SingleElectron.root output_el_2016pre/SingleElectron_HLTEle25.root data_obs HLTEle25
-./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/DY.root output_el_2016pre/DY_HLTEle25.root DY HLTEle25
+# ./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/SingleElectron.root output_el_2016pre/SingleElectron_HLTEle25.root data_obs HLTEle25
+# ./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/DY.root output_el_2016pre/DY_HLTEle25.root DY HLTEle25
 
 #./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/SingleElectron.root output_el_2016pre/SingleElectron_antiiso.root data_obs antiiso
 #./FinalSelection_el.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_el_2016pre/DY.root output_el_2016pre/DY_antiiso.root DY antiiso
