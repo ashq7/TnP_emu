46f16ba6b46cd31965ef9afdbb22542c5a0cfa18
Ashling Quinn <aislinndream918@gmail.com>
2024-12-18 03:15:24 +0100

    embedding electron submission

---
+++ MiniAODProcessorCode/test/submit_crab_el_embeddedBv1_2016pre.py
@@ -0,0 +1,32 @@
+from CRABClient.UserUtilities import config #, getUsernameFromCRIC
+
+config = config()
+
+config.General.requestName = "Electron_embedded_2016preB_ver1"
+config.General.workArea = "crab_projects"
+config.General.transferLogs = True
+config.General.transferOutputs = True
+
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "run_Electron_Embedded_2016pre.py"
+config.JobType.maxMemoryMB = 4000
+#config.JobType.numCores = 8
+
+config.section_("Data")
+config.Data.inputDataset = '/EmbeddingRun2016-HIPM_B_ver1/ElectronEmbedding-inputDoubleMu_106X_ULegacy_miniAOD-v1/USER'
+config.Data.inputDBS     = 'phys03'
+config.Data.splitting    = 'FileBased'
+config.Data.unitsPerJob  = 1
+
+#config.Data.inputDataset = "/EGamma/Run2018A-UL2018_MiniAODv2_GT36-v1/MINIAOD" 
+config.Data.outLFNDirBase = "/store/user/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/2016pre_el_Embedded/"
+config.Data.outputDatasetTag = "TnP_ntuples_embedded_DataBv1_el2016pre"
+#config.Data.inputDBS = "global"
+#config.Data.splitting = "LumiBased"
+#config.Data.unitsPerJob = 500
+config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
+config.Data.publication = False
+config.Data.ignoreLocality = True
+
+config.Site.storageSite = "T3_CH_CERNBOX"
+config.Site.whitelist = ["T2_*","T3_*"]
---
+++ MiniAODProcessorCode/test/submit_crab_el_embeddedF_2016post.py
@@ -0,0 +1,32 @@
+from CRABClient.UserUtilities import config #, getUsernameFromCRIC
+
+config = config()
+
+config.General.requestName = "Electron_embedded_2016postF"
+config.General.workArea = "crab_projects"
+config.General.transferLogs = True
+config.General.transferOutputs = True
+
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "run_Electron_Embedded_2016post.py"
+config.JobType.maxMemoryMB = 4000
+#config.JobType.numCores = 8
+
+config.section_("Data")
+config.Data.inputDataset = '/EmbeddingRun2016_F/ElectronEmbedding-inputDoubleMu_106X_ULegacy_miniAOD-v1/USER'
+config.Data.inputDBS     = 'phys03'
+config.Data.splitting    = 'FileBased'
+config.Data.unitsPerJob  = 1
+
+#config.Data.inputDataset = "/EGamma/Run2018A-UL2018_MiniAODv2_GT36-v1/MINIAOD" 
+config.Data.outLFNDirBase = "/store/user/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/2016post_el_Embedded/"
+config.Data.outputDatasetTag = "TnP_ntuples_embedded_DataF_el2016post"
+#config.Data.inputDBS = "global"
+#config.Data.splitting = "LumiBased"
+#config.Data.unitsPerJob = 500
+config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
+config.Data.publication = False
+config.Data.ignoreLocality = True
+
+config.Site.storageSite = "T3_CH_CERNBOX"
+config.Site.whitelist = ["T2_*","T3_*"]
