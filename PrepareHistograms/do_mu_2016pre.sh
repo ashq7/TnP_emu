bf5b9dc5ecc7b411b5f004414ab80195f921a7c4
Ashling Quinn <aislinndream918@gmail.com>
2024-12-26 14:35:51 +0100

    added 2016 embedding to prepare histo scripts

--- PrepareHistograms/do_mu_2016pre.sh
+++ PrepareHistograms/do_mu_2016pre.sh
@@ -1,9 +1,11 @@
 ./Make.sh FinalSelection_mu.cc
 ./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/SingleMuon.root output_mu_2016pre/Data_HLTMu8Ele23.root data_obs HLTMu8Ele23
 ./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/DY.root output_mu_2016pre/DY_HLTMu8Ele23.root DY HLTMu8Ele23
+./FinalSelection_mu.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Embedded_Muon_2016pre.root output_mu_2016pre/Embedded_HLTMu8Ele23.root data_obs HLTMu8Ele23.root
 
 ./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/SingleMuon.root output_mu_2016pre/Data_HLTMu23Ele12.root data_obs HLTMu23Ele12
 ./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/DY.root output_mu_2016pre/DY_HLTMu23Ele12.root DY HLTMu23Ele12
+./FinalSelection_mu.exe 2016pre /eos/user/a/aquinn/Ha1a2bbtautau_05Jul24/CrabOutput/Embedded_Muon_2016pre.root output_mu_2016pre/Embedded_HLTMu23Ele12.root data_obs HLTMu23Ele12.root
 
 #./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/SingleMuon.root output_mu_2016pre/SingleMuon_antiiso.root data_obs antiiso
 #./FinalSelection_mu.exe 2016pre /eos/cms/store/group/cmst3/group/taug2/TnP_mu_2016pre/DY.root output_mu_2016pre/DY_antiiso.root DY antiiso
