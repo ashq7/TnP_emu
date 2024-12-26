032ac4da98a57cc49744ab26e15f07dccdd048b8
Ashling Quinn <aislinndream918@gmail.com>
2024-12-26 15:38:35 +0100

    tweaks to run Fits with embedded

--- Fit/do_mu_2016pre.sh
+++ Fit/do_mu_2016pre.sh
@@ -1,15 +1,17 @@
 ./Make.sh Fit_mu.cc
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/SingleMuon_HLTMu8Ele23.root output_Data_2016pre_HLTMu8Ele23.root Data HLTMu8Ele23
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu8Ele23.root output_DY_2016pre_HLTMu8Ele23.root DY HLTMu8Ele23
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/Data_HLTMu8Ele23.root output_mu_2016pre/output_Data_2016pre_HLTMu8Ele23.root Data HLTMu8Ele23
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu8Ele23.root output_mu_2016pre/output_DY_2016pre_HLTMu8Ele23.root DY HLTMu8Ele23
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/Embedded_HLTMu8Ele23.root output_mu_2016pre/output_Embedded_2016pre_HLTMu8Ele23.root Embedded HLTMu8Ele23
 python ScaleFactors_mu.py --year=2016pre --discriminant=HLTMu8Ele23
 
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/SingleMuon_HLTMu23Ele12.root output_Data_2016pre_HLTMu23Ele12.root Data HLTMu23Ele12
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu23Ele12.root output_DY_2016pre_HLTMu23Ele12.root DY HLTMu23Ele12
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/Data_HLTMu23Ele12.root output_mu_2016pre/output_Data_2016pre_HLTMu23Ele12.root Data HLTMu23Ele12
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu23Ele12.root output_mu_2016pre/output_DY_2016pre_HLTMu23Ele12.root DY HLTMu23Ele12
+./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/Embedded_HLTMu23Ele12.root output_mu_2016pre/output_Embedded_2016pre_HLTMu23Ele12.root Embedded HLTMu23Ele12
 python ScaleFactors_mu.py --year=2016pre --discriminant=HLTMu23Ele12
 
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/SingleMuon_HLTMu20Tau27.root output_Data_2016pre_HLTMu20Tau27.root Data HLTMu20Tau27
-./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu20Tau27.root output_DY_2016pre_HLTMu20Tau27.root DY HLTMu20Tau27
-python ScaleFactors_mu.py --year=2016pre --discriminant=HLTMu20Tau27
+# ./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/SingleMuon_HLTMu20Tau27.root output_Data_2016pre_HLTMu20Tau27.root Data HLTMu20Tau27
+# ./Fit_mu.exe 2016pre ../PrepareHistograms/output_mu_2016pre/DY_HLTMu20Tau27.root output_DY_2016pre_HLTMu20Tau27.root DY HLTMu20Tau27
+# python ScaleFactors_mu.py --year=2016pre --discriminant=HLTMu20Tau27
 
 #./Make.sh Fit_mu_CMSShape.cc
 #./Fit_mu_CMSShape.exe 2016pre ../PrepareHistograms/output_mu_2016pre/SingleMuon_antiiso.root output_Data_2016pre_antiiso.root Data antiiso
