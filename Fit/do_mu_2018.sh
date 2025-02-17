ad08f54500beb40d4ed77b6f4bde20e4b44c353f
Ashling Quinn <aislinndream918@gmail.com>
2025-02-17 22:28:19 +0100

    trying to output to website

--- Fit/do_mu_2018.sh
+++ Fit/do_mu_2018.sh
@@ -10,6 +10,9 @@ python ScaleFactors_mu.py --year=2018 --discriminant=HLTMu8Ele23
 ./Fit_mu.exe 2018 ../PrepareHistograms/output_mu_2018/Embedded_HLTMu23Ele12.root output_mu_2018/output_Embedded_2018_HLTMu23Ele12.root Embedded HLTMu23Ele12
 python ScaleFactors_mu.py --year=2018 --discriminant=HLTMu23Ele12
 
+cp plots_mu_2018/*.png /eos/user/a/aquinn/www/Ha1a2bbtautau/Fit/plots_mu_2018/.
+cp plots_mu_2018/*.pdf /eos/user/a/aquinn/www/Ha1a2bbtautau/Fit/plots_mu_2018/.
+
 # ./Fit_mu.exe 2018 ../PrepareHistograms/output_mu_2018/SingleMuon_HLTMu20Tau27.root output_Data_2018_HLTMu20Tau27.root Data HLTMu20Tau27
 # ./Fit_mu.exe 2018 ../PrepareHistograms/output_mu_2018/DY_HLTMu20Tau27.root output_DY_2018_HLTMu20Tau27.root DY HLTMu20Tau27
 # python ScaleFactors_mu.py --year=2018 --discriminant=HLTMu20Tau27
